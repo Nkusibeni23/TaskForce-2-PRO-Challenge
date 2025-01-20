f02940be54319295e38d490f62dd1ea5e52ed173
Ndizihiwe Benny Chrispin <benny.ndizihiwe@amalitechtraining.org>
2025-01-20 18:05:51 +0200

    fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import { useRouter } from "next/router";
+"use client";
 
-const NotFoundPage = () => {
-  const router = useRouter();
+import Link from "next/link";
 
-  const handleGoHome = () => {
-    router.push("/");
-  };
-
+export default function NotFound() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
       <div className="max-w-lg p-6 bg-white rounded-lg shadow-lg">
@@ -15,15 +10,13 @@ const NotFoundPage = () => {
         <p className="mt-4 text-xl text-gray-600">
           Oops! The page you were looking for doesn&apos;t exist.
         </p>
-        <button
-          onClick={handleGoHome}
-          className="mt-6 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        <Link
+          href="/"
+          className="mt-6 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 inline-block"
         >
           Go Back to Home
-        </button>
+        </Link>
       </div>
     </div>
   );
-};
-
-export default NotFoundPage;
+}
