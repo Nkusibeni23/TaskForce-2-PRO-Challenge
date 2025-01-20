76ebdeaef56cc025957eccc978f91dd4c0540e7d
Ndizihiwe Benny Chrispin <benny.ndizihiwe@amalitechtraining.org>
2025-01-20 16:32:03 +0200

    fix

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
@@ -7,28 +5,29 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
-
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const AlertDialogPortal = ({
   className,
   children,
   ...props
-}: AlertDialogPrimitive.AlertDialogPortalProps) => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">
-      {children} {/* Now rendering children */}
+}: React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Portal> & {
+  className?: string;
+}) => (
+  <AlertDialogPrimitive.Portal {...props}>
+    <div
+      className={`fixed inset-0 z-50 flex items-end justify-center sm:items-center ${className}`}
+    >
+      {children}
     </div>
   </AlertDialogPrimitive.Portal>
 );
-
 AlertDialogPortal.displayName = AlertDialogPrimitive.Portal.displayName;
 
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Overlay
     className={cn(
       "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm transition-opacity animate-in fade-in",
